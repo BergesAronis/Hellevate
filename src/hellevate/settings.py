d6f2202a402d275d11664ddda5f1de7834694be3
BergesIrani <iraniber@hotmail.com>
2019-05-06 20:49:24 -0400

    Static settings change

--- src/hellevate/settings.py
+++ src/hellevate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'hellevate.herokuapp.com']
 
 
 # Application definition
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    'clubcard/static/',
+]
 
 try:
     from hellevate.local_settings import *
